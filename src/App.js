0928ec1cc32583942ed6a7f41077edfd83e0bbc7
Haley <fougerehaley@gmail.com>
2024-09-25 12:02:22 -0300

    updated title and favicon. Added input and button for new tasks

--- src/App.js
+++ src/App.js
@@ -2,9 +2,36 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // State to manage tasks
+  // useState is a React Hook
+  const [tasks, setTasks] = useState([]); // tasks is a state variable, setTasks is a setter function
+  const [newTask, setNewTask] = useState(''); // holds text for a new task
+  
+  // add a new task
+  const addTask = () => {
+    // trim to check if there is whitespace (or just spaces/empty task)
+    if (newTask.trim()) {
+      //if not an empty string
+      // add a new task to the array of tasks
+      setTasks([...tasks, {text: newTask, completed: false}]);
+      // reset the input field
+      setNewTask('');
+    }
+  };
+  
   return (
     <div className='App-header'>
       <h1>Task Manager</h1>
+      {/* add task input text and button */}
+      <div className='task-input'>
+        <input 
+          type='text'
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder='Enter new task'
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
     </div>
   );
 }
